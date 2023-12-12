99771e17eee5fae3878a98d281ade225cca5e0f4
Matthew Owen <owemj004@mymail.unisa.edu.au>
2023-12-12 22:31:23 +1030

    5 - if you give a seed a fertilizer (part two)

--- 5-if-you-give-a-seed-a-fertilizer/main.py
+++ 5-if-you-give-a-seed-a-fertilizer/main.py
@@ -1,27 +1,39 @@
-SUBJECT_NAMES = ["seed", "soil", "fertilizer", "water", "light", "temperature", "humidity", "location"]
-
-def main() -> None:
-    with open("input.txt") as file:
-        almanac: dict[str, dict[tuple[int, int], tuple[int, int]]] = {}
-        subjects = file.read().split("\n\n")
-        seeds = [int(s) for s in subjects.pop(0).split(": ")[1].split(" ")]
-        for subject in subjects:
-            name, src_dest_maps_str = subject.split(" map:\n")
-            almanac[name] = {}
-            for src_dest_map_str in src_dest_maps_str.split("\n"):
-                dest, src, length = (int(s) for s in src_dest_map_str.split(" "))
-                almanac[name][(src, src + length - 1)] = (dest, dest + length - 1)
-        locations = []
-        for seed in seeds:
-            src_val = seed
-            for i in range(len(SUBJECT_NAMES) - 1):
-                for src_range, dest_range in almanac[SUBJECT_NAMES[i] + "-to-" + SUBJECT_NAMES[i + 1]].items():
-                    if src_range[0] <= src_val <= src_range[1]:
-                        src_val = dest_range[0] + src_val - src_range[0]
-                        break
-            locations.append(src_val)
-        print("Part One:", min(locations))
+seeds, *almanac = open("input.txt").read().split("\n\n")
+seeds = [int(s) for s in seeds.split()[1:]]
+almanac = {name: {(int(src), int(src) + int(length) - 1): int(dest) - int(src)
+                  for dest, src, length in (map.split() for map in maps.split("\n") if map)}
+           for mapping in almanac for name, maps in [mapping.split(" map:\n")]}
 
 
-if __name__ == "__main__":
-    main()
+def get_location_single(seed: int) -> int:
+    for mappings in almanac.values():
+        for (m_st, m_ed), diff in mappings.items():
+            if m_st < seed < m_ed:
+                seed += diff
+                break
+    return seed
+
+
+def get_location_range(seed: tuple[int, int]) -> int:
+    ranges = {seed}
+    for (m_st, m_ed), diff in {mapping: difference for mappings in almanac.values()
+                               for mapping, difference in mappings.items()}.items():
+        new_ranges = set()
+        while ranges:
+            s_st, s_ed = ranges.pop()
+            before = (s_st, min(s_ed, m_st))
+            inter = (max(s_st, m_st), min(s_ed, m_ed))
+            after = (max(s_st, m_ed), s_ed)
+            if before[1] >= before[0]:
+                new_ranges.add(before)
+            if inter[1] >= inter[0]:
+                new_ranges.add((inter[0] + diff, inter[1] + diff))
+            if after[1] >= after[0]:
+                new_ranges.add(after)
+        if new_ranges:
+            ranges = new_ranges
+    return min(st for st, _ in ranges)
+    
+
+print("Part One", min(get_location_single(seed) for seed in seeds))
+print("Part Two", min(get_location_range((seeds[i], seeds[i] + seeds[i + 1])) for i in range(len(seeds) - 1)))
