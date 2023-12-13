a36d68a4beebb2590c256696b38c8b9fde834e9e
Matthew Owen <matthew.owen@consilium.technology>
2023-12-14 08:46:33 +1030

    6 - wait for it (part two)

--- 6-wait-for-it/main.py
+++ 6-wait-for-it/main.py
@@ -1,13 +1,12 @@
-durations, distances = ([int(s) for s in line.split()[1:] if s] for line in open("input.txt").read().split("\n"))
+durations, distances = ([s for s in line.split()[1:] if s] for line in open("input.txt").read().split("\n"))
 
 
-ways_to_win_product = 1
-for duration, distance in zip(durations, distances):
-    ways_to_win = 0
-    for i in range(duration):
-        if i * (duration - i) > distance:
-            ways_to_win += 1
-    ways_to_win_product *= ways_to_win
+def product_of_ways_to_win(duration_distance_pairs: list[tuple[int, int]]) -> int:
+    prod = 1
+    for dur, dist in duration_distance_pairs:
+        prod *= len([0 for vel in range(dur) if vel * (dur - vel) > dist])
+    return prod
 
 
-print("Part One:", ways_to_win_product)
+print("Part One:", product_of_ways_to_win(zip([int(d) for d in durations], [int(d) for d in distances])))
+print("Part Two:", product_of_ways_to_win([(int(''.join(durations)), int(''.join(distances)))]))
