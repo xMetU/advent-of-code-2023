03d4db4d82214124b70ba2d4166f847f165a7692
Matthew Owen <owemj004@mymail.unisa.edu.au>
2023-12-14 20:33:18 +1030

    8 - haunted wasteland (part two)

--- 8-haunted-wasteland/main.py
+++ 8-haunted-wasteland/main.py
@@ -1,13 +1,29 @@
+from math import lcm
+
 instructions, network = open("input.txt").read().split("\n\n")
 network = {key: tuple(value.replace("(", "").replace(")", "").split(", "))
            for key, value in (line.split(" = ") for line in network.split("\n"))}
 
 
-count = 0
-current = "AAA"
-while current != "ZZZ":
-    current = network[current][0] if instructions[count % (len(instructions))] == "L" else network[current][1]
-    count += 1
+def next_node(node: str, step: str) -> str:
+    return network[node][0] if step == "L" else network[node][1]
 
 
-print("Part One:", count)
+step_count = 0
+cur_node = "AAA"
+while cur_node != "ZZZ":
+    cur_node = next_node(cur_node, instructions[step_count % (len(instructions))])
+    step_count += 1
+print("Part One:", step_count)
+
+
+step_count = 0
+cur_nodes = [node for node in network.keys() if node[-1] == "A"]
+step_counts = []
+while cur_nodes:
+    index = instructions[step_count % (len(instructions))]
+    cur_nodes = [next_node(node, index) for node in cur_nodes]
+    step_count += 1
+    step_counts.extend(step_count for node in cur_nodes if node[-1] == "Z")
+    cur_nodes = [node for node in cur_nodes if node[-1] != "Z"]
+print("Part Two:", lcm(*step_counts))
