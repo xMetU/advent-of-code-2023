08639db7bbbb0689142016849d6ec2fa4dc521ab
Matthew Owen <owemj004@mymail.unisa.edu.au>
2023-12-14 18:28:16 +1030

    7 - camel cards (part two)

--- 7-camel-cards/main.py
+++ 7-camel-cards/main.py
@@ -1,10 +1,12 @@
-card_strengths = {"2": 1, "3": 2, "4": 3, "5": 4, "6": 5, "7": 6, "8": 7,
+CARD_STRENGTHS = {"2": 1, "3": 2, "4": 3, "5": 4, "6": 5, "7": 6, "8": 7,
                   "9": 8, "T": 9, "J": 10, "Q": 11, "K": 12, "A": 13}
+CARD_STRENGTHS_WITH_JOKER = {**CARD_STRENGTHS, "J": 0}
+
 
 hand_bid_pairs = [(hand, int(bid)) for hand, bid in [line.split() for line in open("input.txt").read().split("\n")]]
 
 
-def get_strength(hand: str) -> tuple[int, tuple[int, int, int, int, int]]:
+def get_strength(hand: str, card_strengths: dict[str, int] = CARD_STRENGTHS) -> tuple[int, tuple[int, int, int, int, int]]:
     match sorted([hand.count(c) for c in card_strengths.keys() if c in hand]):
         case [5]: hand_strength = 6
         case [1, 4]: hand_strength = 5
@@ -16,5 +18,17 @@ def get_strength(hand: str) -> tuple[int, tuple[int, int, int, int, int]]:
     return (hand_strength, tuple(card_strengths[c] for c in hand))
 
 
-print("Part One:", sum((rank + 1) * bid for rank, (_, bid)
-                       in enumerate(sorted(hand_bid_pairs, key=lambda hbp: get_strength(hbp[0])))))
+def get_strength_with_jokers(hand: str) -> tuple[int, tuple[int, int, int, int, int]]:
+    if "J" not in hand:
+        return get_strength(hand)
+    counts = {c: hand.count(c) for c in hand if c != "J"}
+    candidate = max([k for k, v in counts.items() if v == max(counts.values())],
+                     key=CARD_STRENGTHS_WITH_JOKER.get) if counts else "A"
+    return (get_strength(hand.replace("J", candidate))[0], get_strength(hand, CARD_STRENGTHS_WITH_JOKER)[1])
+    
+
+hand_bid_pairs = sorted(hand_bid_pairs, key=lambda hbp: get_strength(hbp[0]))
+print("Part One:", sum((rank + 1) * bid for rank, (_, bid) in enumerate(hand_bid_pairs)))
+
+hand_bid_pairs = sorted(hand_bid_pairs, key=lambda hbp: get_strength_with_jokers(hbp[0]))
+print("Part Two:", sum((rank + 1) * bid for rank, (_, bid) in enumerate(hand_bid_pairs)))
