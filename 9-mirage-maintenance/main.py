c528da3cebc6a6a2a91695bb90bf207ee0d68b83
Matthew Owen <owemj004@mymail.unisa.edu.au>
2023-12-15 16:58:31 +1030

    9 - mirage maintenance (part two)

--- 9-mirage-maintenance/main.py
+++ 9-mirage-maintenance/main.py
@@ -10,4 +10,14 @@ def get_next_value(history: list[int]) -> int:
     return sequences[0][-1]
 
 
+def get_prev_value(history: list[int]) -> int:
+    sequences = [history]
+    while not all(n == 0 for n in sequences[-1]):
+        sequences.append([sequences[-1][i + 1] - sequences[-1][i] for i in range(len(sequences[-1]) - 1)])
+    for i in range(len(sequences) - 2, -1, -1):
+        sequences[i] = [sequences[i][0] - sequences[i + 1][0]] + sequences[i]
+    return sequences[0][0]
+
+
 print("Part One:", sum(get_next_value(history) for history in histories))
+print("Part Two:", sum(get_prev_value(history) for history in histories))
